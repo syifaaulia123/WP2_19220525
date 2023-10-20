41b58b58998eb8357b14a7a2529605b72eb5c056
syifaaulia123 <syifa.au89@gmail.com>
2023-10-20 14:39:20 +0700

    menambahkan file application dan assets

---
+++ application/views/V_about.php
@@ -0,0 +1,32 @@
+<h1><?php echo $judul ?></h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>RentalBuku</title>
+</head>
+<body>
+<img width=220 height=250  src=" <?= base_url('assets/img/syifa.jpg') ?> ">
+<section>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Syifa </li>
+        <li>Nama Belakang : Aulia</li>
+    </ul>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> Bekasi, Puri Harapan</li>
+    </ul>
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Bekasi</li>
+    </ul>
+    <h4>Hobi</h4>
+    <ul type="square">
+        <li>Kulineran</li>
+        <li>Masak</li>
+    </ul>
+</section>
+</body>
+</html>
